f1130bc9e02756423f772b7db387b12c25d6db2f
elrouby <adhelrouby@gmail.com>
2024-03-25 16:53:52 +0200

    Added rotation animation

--- player.cpp
+++ player.cpp
@@ -8,8 +8,8 @@
 Player::Player() {
     setPixmap(QPixmap(":/images/img/player.png").scaled(100, 100));
     setTransformOriginPoint(50, 50);
+    rotAngle = 15;
 }
-
 
 void Player::keyPressEvent(QKeyEvent* event) {
     // *******  Event Handling for the Player ********
@@ -19,13 +19,13 @@ void Player::keyPressEvent(QKeyEvent* event) {
         // to prevent the player from getting out of the screen
         if(x()>0) {
             setPos(x()-offset,y());
-            setRotation(-rotAngle);
+            animateRot(-rotAngle);
         }
     } else if(event->key()== Qt::Key_Right) {
         // to prevent the player from getting out of the screen
         if(x() + 100 < 800) {
             setPos(x()+offset,y());
-            setRotation(rotAngle);
+            animateRot(rotAngle);
         }
     } else if(event->key()== Qt::Key_Space) {
         Bullet* bullet = new Bullet();
@@ -36,8 +36,18 @@ void Player::keyPressEvent(QKeyEvent* event) {
 }
 
 void Player::keyReleaseEvent(QKeyEvent* event) {
-    if(event->key())
-        setRotation(0);
+    if(event->key() == Qt::Key_Left || event->key() == Qt::Key_Right)
+        animateRot(0);
+}
+
+void Player::animateRot(qreal endRotation) {
+    QPropertyAnimation *animation = new QPropertyAnimation(this, "rotation");
+    int durTime = 150;
+    animation->setDuration(durTime); // Animation duration in millisseconds
+    animation->setStartValue(rotation());
+    animation->setEndValue(endRotation);
+    animation->setEasingCurve(QEasingCurve::InOutQuad); // Use quadratic easing for smoother animation
+    animation->start(QAbstractAnimation::DeleteWhenStopped);
 }
 
 void Player::playBulletSound() {
--- player.h
+++ player.h
@@ -2,14 +2,19 @@
 #define PLAYER_H
 #include <QGraphicsPixmapItem>
 #include <QObject>
+#include <QPropertyAnimation>
 
 class Player : public QObject, public QGraphicsPixmapItem {
     Q_OBJECT
+    Q_PROPERTY(qreal rotation READ rotation WRITE setRotation)
 public:
     Player();
     void keyPressEvent(QKeyEvent* event);
     void keyReleaseEvent(QKeyEvent* event);
     void playBulletSound();
+private:
+    void animateRot(qreal endRotation);
+    qreal rotAngle;
 };
 
 #endif // PLAYER_H
